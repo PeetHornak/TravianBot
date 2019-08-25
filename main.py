8a05846a3c5e102f9086feb547caeefa78890cb5
peter.hornak <peter.hornak@kiwi.com>
2019-08-25 20:23:57 +0200

    Im dumb

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ def main():
     url = os.environ.get(f'VILLAGE_URL_{village_number}')
     while url:
         loop.create_task(builders_manager(url, village_number))
-        # loop.create_task(troop_raid(url, village_number))
+        loop.create_task(troop_raid(url, village_number))
         village_number += 1
         url = os.environ.get(f'VILLAGE_URL_{village_number}')
 
